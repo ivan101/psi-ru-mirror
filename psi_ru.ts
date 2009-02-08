ebce078fc5147885b6ab9aefd911dc89d20fcf92
Ivan Tyumentsev <givan101@gmail.com>
2009-02-08 11:37:07 +0000

    Translated Jingle

--- psi_ru.ts
+++ psi_ru.ts
@@ -128,7 +128,7 @@ Copyright © 2001-2008 The Psi Team.&lt;br&gt;</translation>
 Join the translation team today! Go to 
 &lt;a href=&quot;http://forum.psi-im.org/forum/14&quot;&gt;
 http://forum.psi-im.org/forum/14&lt;/a&gt; for further details!</source>
-        <translation>Иван Тюменцев&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ivan101@users.sourceforge.net&quot;&gt;ivan101@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Переводчик&lt;br&gt;&lt;br&gt;Михаил Пищагин&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:mblsha@users.sourceforge.net&quot;&gt;mblsha@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Антон Фарыгин (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:rider@altlinux.com&quot;&gt;rider@altlinux.com&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Андрей Черепанов (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:cas@altlinux.ru&quot;&gt;cas@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение и вычистка перевода&lt;br&gt;&lt;br&gt;Валентина Ванеева (ALT Linux Team)&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:org@altlinux.ru&quot;&gt;org@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Константин Исаков при содействии подписчиков списка рассылки &lt;a href=&quot;mailto:jabber-users@jabber.ru&quot;&gt;jabber-users@jabber.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ikm@users.sf.net&quot;&gt;ikm@users.sf.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Оригинальный перевод&lt;br&gt;&lt;br&gt;Присоединяйся к команде перевода! Подробности в конференции &lt;a href=&quot;xmpp:psi-dev@conference.jabber.ru?join&quot;&gt;psi-dev@conference.jabber.ru&lt;/a&gt;&lt;br&gt;&lt;br&gt;Свежая версия перевода всегда тут: &lt;a href=&quot;http://code.google.com/p/psi-ru/downloads/list&quot;&gt;http://psi-ru.googlecode.com&lt;/a&gt;&lt;br&gt;©29.01.2009</translation>
+        <translation>Иван Тюменцев&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ivan101@users.sourceforge.net&quot;&gt;ivan101@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Переводчик&lt;br&gt;&lt;br&gt;Михаил Пищагин&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:mblsha@users.sourceforge.net&quot;&gt;mblsha@users.sourceforge.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Антон Фарыгин (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:rider@altlinux.com&quot;&gt;rider@altlinux.com&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Андрей Черепанов (ALT Linux) &lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:cas@altlinux.ru&quot;&gt;cas@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение и вычистка перевода&lt;br&gt;&lt;br&gt;Валентина Ванеева (ALT Linux Team)&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:org@altlinux.ru&quot;&gt;org@altlinux.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Дополнение перевода&lt;br&gt;&lt;br&gt;Константин Исаков при содействии подписчиков списка рассылки &lt;a href=&quot;mailto:jabber-users@jabber.ru&quot;&gt;jabber-users@jabber.ru&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;&lt;a href=&quot;mailto:ikm@users.sf.net&quot;&gt;ikm@users.sf.net&lt;/a&gt;&lt;br&gt;&amp;nbsp;&amp;nbsp;Оригинальный перевод&lt;br&gt;&lt;br&gt;Присоединяйся к команде перевода! Подробности в конференции &lt;a href=&quot;xmpp:psi-dev@conference.jabber.ru?join&quot;&gt;psi-dev@conference.jabber.ru&lt;/a&gt;&lt;br&gt;&lt;br&gt;Свежая версия перевода всегда тут: &lt;a href=&quot;http://code.google.com/p/psi-ru/downloads/list&quot;&gt;http://psi-ru.googlecode.com&lt;/a&gt;&lt;br&gt;©08.02.2009</translation>
     </message>
     <message>
         <location filename="../src/aboutdlg.cpp" line="70"/>
@@ -1426,22 +1426,22 @@ Make sure you enter a fully qualified Jabber ID.</source>
     <message>
         <location filename="../src/avcall/call.ui" line="13"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Диалог</translation>
     </message>
     <message>
         <location filename="../src/avcall/call.ui" line="19"/>
         <source>Call status</source>
-        <translation type="unfinished"></translation>
+        <translation>Статус вызова</translation>
     </message>
     <message>
         <location filename="../src/avcall/call.ui" line="54"/>
         <source>&amp;Accept</source>
-        <translation type="unfinished">&amp;Принять</translation>
+        <translation>&amp;Принять</translation>
     </message>
     <message>
         <location filename="../src/avcall/call.ui" line="61"/>
         <source>&amp;Reject</source>
-        <translation type="unfinished">&amp;Отклонить</translation>
+        <translation>&amp;Отклонить</translation>
     </message>
 </context>
 <context>
@@ -1764,67 +1764,67 @@ Do you still want to close the window?</source>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="13"/>
         <source>Dialog</source>
-        <translation type="unfinished"></translation>
+        <translation>Диалог</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="19"/>
         <source>Devices</source>
-        <translation type="unfinished"></translation>
+        <translation>Устройства</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="25"/>
         <source>Audio output:</source>
-        <translation type="unfinished"></translation>
+        <translation>Аудиовыход:</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="44"/>
         <source>Send live stream</source>
-        <translation type="unfinished"></translation>
+        <translation>Отправить живой эфир</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="51"/>
         <source>Send file</source>
-        <translation type="unfinished">Отправить файл</translation>
+        <translation>Отправить файл</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="60"/>
         <source>Audio input:</source>
-        <translation type="unfinished"></translation>
+        <translation>Аудиовход:</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="77"/>
         <source>Video input:</source>
-        <translation type="unfinished"></translation>
+        <translation>Видеовход:</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="94"/>
         <source>File:</source>
-        <translation type="unfinished">Файл:</translation>
+        <translation>Файл:</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="106"/>
         <source>...</source>
-        <translation type="unfinished">...</translation>
+        <translation>...</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="115"/>
         <source>Loop file playback</source>
-        <translation type="unfinished"></translation>
+        <translation>Повтор воспроизведения файлов</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="125"/>
         <source>Modes</source>
-        <translation type="unfinished"></translation>
+        <translation>Режимы</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="131"/>
         <source>Audio:</source>
-        <translation type="unfinished"></translation>
+        <translation>Аудио:</translation>
     </message>
     <message>
         <location filename="../../psimedia/demo/config.ui" line="148"/>
         <source>Video:</source>
-        <translation type="unfinished"></translation>
+        <translation>Видео:</translation>
     </message>
 </context>
 <context>
@@ -1832,17 +1832,17 @@ Do you still want to close the window?</source>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="354"/>
         <source>Configure Audio/Video</source>
-        <translation type="unfinished"></translation>
+        <translation>Настройка аудио/видео</translation>
     </message>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="491"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Открыть файл</translation>
     </message>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="493"/>
         <source>Ogg Audio/Video (*.oga *.ogv *.ogg)</source>
-        <translation type="unfinished"></translation>
+        <translation>Ogg аудио/видео (*.oga *.ogv *.ogg)</translation>
     </message>
 </context>
 <context>
@@ -2163,7 +2163,7 @@ Proceed?</source>
     <message>
         <location filename="../src/contactview.cpp" line="1255"/>
         <source>Voice Call</source>
-        <translation>Голосовой звонок<byte value="x9"/></translation>
+        <translation>Позвонить</translation>
     </message>
     <message>
         <location filename="../src/contactview.cpp" line="1116"/>
@@ -4689,12 +4689,12 @@ Are you sure you want to discard them?</source>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="1612"/>
         <source>PsiMedia Test</source>
-        <translation type="unfinished"></translation>
+        <translation>PsiMedia тест</translation>
     </message>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="1615"/>
         <source>Error: Could not load PsiMedia subsystem.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка: Не удалось загрузить подсистему PsiMedia.</translation>
     </message>
 </context>
 <context>
@@ -4702,39 +4702,41 @@ Are you sure you want to discard them?</source>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="1067"/>
         <source>System error</source>
-        <translation type="unfinished"></translation>
+        <translation>Системная ошибка</translation>
     </message>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="1069"/>
         <source>Codec error</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка кодека</translation>
     </message>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="1071"/>
         <source>Generic error</source>
-        <translation type="unfinished"></translation>
+        <translation>Типовая ошибка</translation>
     </message>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="1411"/>
         <source>Error</source>
-        <translation type="unfinished">Ошибка</translation>
+        <translation>Ошибка</translation>
     </message>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="1173"/>
         <source>Cannot send live without at least one audio input or video input device selected.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не удается отправить эфир, выберите, по крайней мере, одно аудио или видео устройство входа.</translation>
     </message>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="1397"/>
         <source>An error occurred while trying to send:
 %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка при попытке отправить:
+%1.</translation>
     </message>
     <message>
         <location filename="../src/avcall/jinglertp.cpp" line="1413"/>
         <source>An error occurred while trying to receive:
 %1.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибка при попытке получить:
+%1.</translation>
     </message>
 </context>
 <context>
@@ -5258,7 +5260,7 @@ Reason: %1</source>
     <message>
         <location filename="../src/mainwin.cpp" line="411"/>
         <source>Ctrl+Shift+N</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+Shift+N</translation>
     </message>
 </context>
 <context>
